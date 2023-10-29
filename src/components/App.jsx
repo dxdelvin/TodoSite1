eeaac8ad79789665d2064299e7c6d0fc767203a0
dxdelvin <dxdelvin@gmail.com>
2023-10-30 01:21:54 +0530

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+import Task from "./Task";
+
+
+
+export default function App(){
+    const [text, setText] = useState("")
+    const [items, setItems]= useState([])
+
+    function recordChange(event){
+        const value = event.target.value
+        setText(value)
+    }
+
+    function handleChange(){
+        setItems(items.concat(<Task taskadd={text}/>))
+        setText("")
+    }
+    
+    return (<div>
+    <div className="center-flex">
+        <div className="center-rectangle">
+            <h1 className="heading">To-Do List</h1>
+            <div>
+                <input onChange={recordChange} value={text} type="text" name="tasktext"></input>
+                <button onClick={handleChange} id="add">ADD</button>
+            </div>
+            <div className="todo-items">
+                <ul>
+                <Task taskadd="Henry Sir Op"/>
+                {items}
+                </ul>
+            </div>
+        </div>
+    </div>
+    </div>);
+}
+
